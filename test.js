6023e00a860f45ca66f6d75615fc9394ce8affba
Fuyang Liu <fuyangl@spotify.com>
2020-02-09 17:07:57 +0100

    Add a minor example change on test.js

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ console.log(RB62);
 
 RB62.wasm.then(() => {
     console.log(wasm_bindgen.sum(1, 2));
+    console.log(wasm_bindgen.get_b62("dbc3d5ebe344484da3e2448712a02213"))
     console.log(wasm_bindgen.get_integer("6GGODyP2LIdbxIfYxy5UbN"))
     console.log(wasm_bindgen.get_integer_batch(["6GGODyP2LIdbxIfYxy5UbN", "6GGODyP2LIdbxIfYxy5Ub2", "6GGODyP2LIdbxIfYxy5Ub3"]))
 });
